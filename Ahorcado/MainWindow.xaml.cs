6b56e78b142d77f942579b3e6c2ad2bc41483b99
jose <jl.rodriguez@iesdoctorbalmis.com>
2021-10-22 11:54:43 +0200

    Añadido teclado

--- Ahorcado/MainWindow.xaml.cs
+++ Ahorcado/MainWindow.xaml.cs
@@ -28,10 +28,10 @@ namespace Ahorcado
 
             //Hacer un bitmap de imagenes para que nos muestre los cambios
 
-            List<Char> abc = Enumerable.Range('A', 'Z' - 'A' + 1).Select(i => (Char)i).ToList<Char>();
-            abc.Insert(14,'Ñ');
+            List<Char> abcedario = Enumerable.Range('A', 'Z' - 'A' + 1).Select(i => (Char)i).ToList<Char>();
+            abcedario.Insert(14, 'Ñ');
 
-            foreach (var letra in abc)
+            foreach (var letra in abcedario)
             {
                 Button letras = new Button();
                 letras.Tag = letra;
@@ -49,26 +49,44 @@ namespace Ahorcado
 
             //this es para la ventana actual y si queremos hacer referencia al
             //app.xmal usaremos Application.Current.Rs
-        }
 
+            TextBox palabra = new TextBox();
+            palabra.Width = 100;
+            palabra.Height = 100;
+            PalabraWrapPanel.Children.Add(palabra);
+        }
+        
+        //Clicar sobre botón
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //FALTA PONER POR TECLADO
-            Button boton = (Button)sender;               
+
+            Button boton = (Button)sender;
             boton.IsEnabled = false;
         }
 
         private void Reiniciar_Botones(object sender, RoutedEventArgs e)
         {
-            
+
             //Hay que hacer un foreach de los botones que tenemos del children del contenedor 
-            foreach(Button b in LetrasUniformGrid.Children)
+            foreach (Button b in LetrasUniformGrid.Children)
             {
                 b.IsEnabled = true;
             }
         }
 
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            foreach (Button b in LetrasUniformGrid.Children)
+            {
+                if(b.Tag.ToString() == e.Key.ToString() )
+                b.IsEnabled = false;
+            }
+        }
+
         //Añadir letras y letras está en letras , letras acertadas
+
+
     }
 
 
